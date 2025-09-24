673950ee1983c90f885d00eabba08bfec0083e30
JIWON HWANG <155422991+benchprincess@users.noreply.github.com>
2025-09-24 19:29:23 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,20 +1,16 @@
-from collections import deque
-
-
 def solution(cards1, cards2, goal):
-    cards1 = deque(cards1)
-    cards2 = deque(cards2)
-    goal = deque(goal)
-    
-    while goal:
-        if cards1 and cards1[0] == goal[0]:
-            cards1.popleft()
-            goal.popleft()
-        elif cards2 and cards2[0] == goal[0]:
-            cards2.popleft()
-            goal.popleft()
+    cards1_idx = 0
+    cards2_idx = 0
+    goal_count = 0
+
+    while goal_count < len(goal):
+        if cards1_idx < len(cards1) and goal[goal_count] == cards1[cards1_idx]:
+            cards1_idx += 1
+            goal_count += 1
+        elif cards2_idx < len(cards2) and goal[goal_count] == cards2[cards2_idx]:
+            cards2_idx += 1
+            goal_count += 1
         else:
-            break
-            
-    return "Yes" if not goal else "No"
-            
+            return "No"
+
+    return "Yes"
